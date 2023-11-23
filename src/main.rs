e8a8341af213340a14c2ac878ee3f7d1439b35cf
Jack Youstra <jack@youstra.com>
2023-11-23 14:52:32 -0500

    Update names

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Bevy game".to_string(), // ToDo
+                title: "Backpop".to_string(), // ToDo
                 // Bind to canvas included in `index.html`
                 canvas: Some("#bevy".to_owned()),
                 // The canvas size is constrained in index.html and build/web/styles.css
